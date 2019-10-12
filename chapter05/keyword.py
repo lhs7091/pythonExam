45478cdd4a04aeec997f2f3ba8b8c2bbd999438f
lhs7091 <lhs7091@naver.com>
2019-10-12 14:24:42 +0900

    python chapter05
    키워드 인수 정의 및 출력

---
+++ chapter05/keyword.py
@@ -0,0 +1,5 @@
+def print_something(my_name, yuor_name):
+    print("Hello {0}, My name is {1}".format(my_name, yuor_name))
+
+print_something("aaa", "bbb")
+print_something(yuor_name="aaa", my_name="bbb")
