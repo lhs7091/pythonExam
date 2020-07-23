025e4adb742ff78379b9fcf774916282c90d8c3b
lhs7091 <lhs7091@naver.com>
2020-07-23 12:02:43 +0900

    # GUI project by python
    create Frame

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("GUI Start")
+
+#window size(width x hight + position x + position y)
+root.geometry("640x480+300+100")
+
+root.resizable(False, False)
+
+root.mainloop()
